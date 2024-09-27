3eb98425eae851868d5b4ec606bcb22edec1afe4
Peace3B <149768110+Peace3B@users.noreply.github.com>
2024-09-27 10:19:12 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 import uuid
 import models
 from datetime import datetime
+from models import storage
 from sqlalchemy import Column, Integer, String, DateTime
 
 
