29ca440d0ae7f2537dc9085e0f7b674e099efd5a
Peace3B <149768110+Peace3B@users.noreply.github.com>
2024-09-27 10:00:42 +0200

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,6 +8,19 @@ import json
 import os
 
 
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            # Set 'created_at' from kwargs or current time
+            self.created_at = datetime.datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f") if 'created_at' in kwargs else datetime.datetime.now()
+
+            # Set 'updated_at' to the current time regardless of kwargs
+            self.updated_at = datetime.datetime.now()
+        else:
+            # Normal initialization
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+
 class test_basemodel(unittest.TestCase):
     """ """
 
