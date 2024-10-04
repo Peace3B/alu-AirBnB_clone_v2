f4296873cebb6620ba92d8451963b770d3804179
Peace3B <p.keza@alustudent.com>
2024-10-04 11:12:49 +0200

    Harahiye

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-# script that sets up web servers for the deployment of web_static
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
+# Script that sets up web servers for the deployment of web_static
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
+# Install Nginx if it is not already installed
+sudo apt-get update -y
+sudo apt-get install nginx -y
+
+# Create the folder /data/ if it doesn’t already exist
 sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
+sudo mkdir -p /data/web_static/shared/
+
+# Create a fake HTML file /data/web_static/releases/test/index.html
+echo "<html>
   <head>
   </head>
   <body>
@@ -18,10 +18,20 @@ sudo echo "<html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+# Create a symbolic link /data/web_static/current linked to /data/web_static/releases/test/
+if [ -L /data/web_static/current ]; then
+    sudo rm /data/web_static/current
+fi
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 
+# Give ownership of the /data/ folder to the ubuntu user AND group (recursive)
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
+sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
+# Restart Nginx
 sudo service nginx restart
+
+# Exit with a successful status
+exit 0
